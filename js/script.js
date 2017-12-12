9d503b15f7c94e1576b84a2e11f776dd59311562
Taufiq <taufiqabdus@gmail.com>
2017-12-12 23:19:17 +0000

    fixed jquery, rigged cpu choice

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
-//Class 1:
-        // Fork and clone the repo and set up your project workspace and link files
-        // Write HTML for your project
-//Class 2:  
-        // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
-        // Practice using console.log() and debugger to debug your code
-        // Create your document ready function. 
-        // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
-        // Display the user choice to the output screen
-//Class 3:  
-        // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
-        // Display the computer choice to the output screen
-        // Compare the user choice and the computer choice to determine who won. 
-        // Display the user winner to the output screen 
-        // Clear the input box for the next choice
+/*global $*/
 
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
+$(document).ready(function(){
+$("#shoot").click(function(){
+var playChoice=["Rock","Paper","Scissors"];
+var cpuChoice=["Rock","Paper","Scissors"];
 
-//GLOBAL VARIABLES
+var origin=Math.random();
+var choice=origin*3;
+var player= $("input").val();
+var cpu=Math.floor(choice);
+ 
+  if(cpu===0){
+         
+         $("#it").text(cpuChoice[0]);
+         console.log(cpuChoice[0]);
+}
+  else if(cpu===1){
+          
+          $("#it").text(cpuChoice[1]);
+          console.log(cpuChoice[1]);
+          
+}
+
+  else if (cpu===2){
+          
+          $("#it").text(cpuChoice[2]);
+          console.log(cpuChoice[2]);
+}
 
 
 
-//FUNCTIONS
-
-
-
-// DOCUMENT READY FUNCTION
-
+   if(player==="Rock" || player==="rock"){
+           
+           $("#you")
+   } 
+});
+});
